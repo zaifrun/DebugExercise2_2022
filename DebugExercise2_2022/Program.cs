6c2812687a5315ff50b60b5984659400b0b07f53
Martin Knudsen <martinknud@gmail.com>
2022-11-21 11:16:01 +0100

    Add project files.

---
+++ DebugExercise2_2022/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool numberNotFound = false;
+int number = 2520;
+
+while (numberNotFound)
+{
+    for (int divider = 0; divider < 20; divider++)
+    {
+        if (number % divider != 0)
+            break;
+        else
+            numberNotFound = true;
+    }
+    number += 2;
+}
+
+// 232792600 er svaret vi leder efter
+Console.WriteLine(number);
+
+Console.ReadLine();
